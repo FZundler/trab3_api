39074870ed51b5ef0ba3c0014ba7441a45a1f242
Felipe Zündler <felipezundlercontato@gmail.com>
2024-12-04 01:49:14 -0300

    api tokens

--- middewares/verificaToken.ts
+++ middewares/verificaToken.ts
@@ -6,26 +6,31 @@ interface TokenI {
   userLogadoNome: string
 }
 
-export function verificaToken(req: Request | any, res: Response, next: NextFunction) {
+// Estendendo a interface Request para incluir os novos campos
+interface CustomRequest extends Request {
+  userLogadoId?: number
+  userLogadoNome?: string
+}
+
+export function verificaToken(req: CustomRequest, res: Response, next: NextFunction) {
   const { authorization } = req.headers
 
   if (!authorization) {
-    res.status(401).json({ error: "Token não informado" })
-    return
+    return res.status(401).json({ error: "Token não informado" })
   }
 
   const token = authorization.split(" ")[1]
 
   try {
-    const decode = jwt.verify(token, process.env.JWT_KEY as string)
-    // console.log(decode)
-    const { userLogadoId, userLogadoNome } = decode as TokenI
+    // Aqui, estamos garantindo que o processo de verificação vai decodificar o token corretamente
+    const decoded = jwt.verify(token, process.env.JWT_KEY as string) as TokenI
+    const { userLogadoId, userLogadoNome } = decoded
 
-    req.userLogadoId   = userLogadoId
+    req.userLogadoId = userLogadoId
     req.userLogadoNome = userLogadoNome
 
     next()
   } catch (error) {
-    res.status(401).json({ error: "Token inválido" })
+    return res.status(401).json({ error: "Token inválido" })
   }
 }
